b0f2c2adf2b5abb66726835ad381c0460b18761a
William Mora <wilmor24@gmail.com>
2013-03-20 20:27:35 -0430

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server)
+  , _ = require('underscore')
+  , participants = [];
+
+app.set('port', process.env.PORT || 8080);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.bodyParser());
+app.use(express.static('public', __dirname + 'public'));
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.post('/message', function(request, response) {
+
+  var message = request.body.message;
+
+  if(_.isEmpty(message.trim())) {
+    return response.json(400, {error: "Message is empty"});
+  }
+
+  var name = request.body.name;
+
+  io.sockets.emit('incomingMessage', {message: message, name: name});
+  response.send(200);
+
+});
+
+io.on('connection', function(socket){
+
+  socket.on('disconnect', function() {
+    participants = _.without(participants,_.findWhere(participants, {id: socket.id}));
+    io.sockets.emit('userDisconnected', {id: socket.id, sender:'system'});
+  });
+
+  socket.on('newUser', function(data) {
+    participants.push({id: data.id, name: data.name});
+    io.sockets.emit('newConnection', {participants: participants});
+  });
+
+  socket.on('nameChange', function(data) {
+    _.findWhere(participants, {id: socket.id}).name = data.name;
+    io.sockets.emit('nameChanged', {id: data.id, name: data.name});
+  });
+
+});
+
+server.listen(app.get('port'));
---
+++ public/javascripts/index.js
@@ -0,0 +1,85 @@
+function init() {
+  var serverBaseUrl = 'http://localhost:3000';
+  var socket = io.connect(serverBaseUrl);
+  var sessionId = '';
+
+  socket.on('connect', function () {
+    sessionId = socket.socket.sessionid;
+    console.log('Connected ' + sessionId);
+    socket.emit('newUser', {id: sessionId, name: $('#name').val()});
+  });
+
+  socket.on('disconnect', function () {
+    sessionId = socket.socket.sessionid;
+    console.log('Connected ' + sessionId);
+  });
+
+  socket.on('userDisconnected', function(data) {
+    $('#' + data.id).remove();
+  });
+
+  socket.on('nameChanged', function (data) {
+    $('#' + data.id).text(data.name + ' ' + (data.id === sessionId ? '(You)' : ''));
+  });
+
+  socket.on('newConnection', function (data) {
+    $('#participants').html('');
+    var participants = data.participants;
+    for (var i = 0; i < participants.length; i++) {
+      $('#participants').append('<span id="' + participants[i].id + '">' +
+        participants[i].name + ' ' + (participants[i].id === sessionId ? '(You)' : '') + '<br /></span>');
+    }
+  });
+
+  socket.on('incomingMessage', function (data) {
+    var message = data.message;
+    var name = data.name;
+    $('#messages').prepend('<b>' + name + '</b><br />' + message + '<hr />');
+  });
+
+  socket.on('error', function (reason) {
+    console.log('Unable to connect to client', reason);
+  });
+
+  function sendMessage() {
+    var outgoingMessage = $('#outgoingMessage').val();
+    var name = $('#name').val();
+    $.ajax({
+      url: serverBaseUrl + '/message',
+      type: 'POST',
+      dataType: 'json',
+      data: {message: outgoingMessage, name: name}
+    });
+  }
+
+  function outgoingMessageKeyDown(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      if ($('#outgoingMessage').val().trim().length <= 0) {
+        return;
+      }
+      sendMessage();
+      $('#outgoingMessage').val('');
+    }
+  }
+
+  function outgoingMessageKeyUp() {
+    var outgoingMessageValue = $('#outgoingMessage').val();
+
+    $('#send').attr('disabled', (outgoingMessageValue.trim()).length > 0 ? false : true);
+  }
+
+  function nameFocusOut() {
+    var name = $('#name').val();
+    socket.emit('nameChange', {id: sessionId, name: name});
+  }
+
+  $('#outgoingMessage').on('keydown', outgoingMessageKeyDown);
+  $('#outgoingMessage').on('keyup', outgoingMessageKeyUp);
+
+  $('#name').on('focusout', nameFocusOut);
+
+  $('#send').on('click', sendMessage);
+}
+
+$(document).on('ready', init);
