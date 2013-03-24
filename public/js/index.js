2e4b84dfb86d53cbd871082b9a6591e2be9eb0f1
William Mora <wilmor24@gmail.com>
2013-03-23 19:48:01 -0430

    Project refactoring for tutorial

---
+++ public/js/index.js
@@ -0,0 +1,142 @@
+function init() {
+
+  var serverBaseUrl = document.domain;
+
+  /*
+   On client init, try to connect to the socket.IO server.
+   Note we don't specify a port since we set up our server
+   to run on port 8080
+   */
+  var socket = io.connect(serverBaseUrl);
+
+  //We'll save our session ID in a variable for later
+  var sessionId = '';
+
+  //Helper function to update the participants' list
+  function updateParticipants(participants) {
+    $('#participants').html('');
+    for (var i = 0; i < participants.length; i++) {
+      $('#participants').append('<span id="' + participants[i].id + '">' +
+        participants[i].name + ' ' + (participants[i].id === sessionId ? '(You)' : '') + '<br /></span>');
+    }
+  }
+
+  /*
+   When the client successfuly connects to the server, an
+   event "connect" is emitted. Let's get the session ID and
+   log it. Also, let the socket.IO server there's a new user
+   with a session ID and a name. We'll emit the "newUser" event
+   for that.
+   */
+  socket.on('connect', function () {
+    sessionId = socket.socket.sessionid;
+    console.log('Connected ' + sessionId);
+    socket.emit('newUser', {id: sessionId, name: $('#name').val()});
+  });
+
+  /*
+   When the server emits the "newConnection" event, we'll reset
+   the participants section and display the connected clients.
+   Note we are assigning the sessionId as the span ID.
+   */
+  socket.on('newConnection', function (data) {
+    updateParticipants(data.participants);
+  });
+
+  /*
+   When the server emits the "userDisconnected" event, we'll
+   remove the span element from the participants element
+   */
+  socket.on('userDisconnected', function(data) {
+    $('#' + data.id).remove();
+  });
+
+  /*
+   When the server fires the "nameChanged" event, it means we
+   must update the span with the given ID accordingly
+   */
+  socket.on('nameChanged', function (data) {
+    $('#' + data.id).html(data.name + ' ' + (data.id === sessionId ? '(You)' : '') + '<br />');
+  });
+
+  /*
+   We'll get a "newConnection" event when there's a new user. We'll apply
+   the same logic as the "userDisconnected" event
+   */
+  socket.on('newConnection', function (data) {
+    updateParticipants(data.participants);
+  });
+
+  /*
+   When receiving a new chat message with the "incomingMessage" event,
+   we'll prepend it to the messages section
+   */
+  socket.on('incomingMessage', function (data) {
+    var message = data.message;
+    var name = data.name;
+    $('#messages').prepend('<b>' + name + '</b><br />' + message + '<hr />');
+  });
+
+  /*
+   Log an error if unable to connect to server
+   */
+  socket.on('error', function (reason) {
+    console.log('Unable to connect to server', reason);
+  });
+
+  /*
+   "sendMessage" will do a simple ajax POST call to our server with
+   whatever message we have in our textarea
+   */
+  function sendMessage() {
+    var outgoingMessage = $('#outgoingMessage').val();
+    var name = $('#name').val();
+    $.ajax({
+      url:  '/message',
+      type: 'POST',
+      dataType: 'json',
+      data: {message: outgoingMessage, name: name}
+    });
+  }
+
+  /*
+   If user presses Enter key on textarea, call sendMessage if there
+   is something to share
+   */
+  function outgoingMessageKeyDown(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      if ($('#outgoingMessage').val().trim().length <= 0) {
+        return;
+      }
+      sendMessage();
+      $('#outgoingMessage').val('');
+    }
+  }
+
+  /*
+   Helper function to disable/enable Send button
+   */
+  function outgoingMessageKeyUp() {
+    var outgoingMessageValue = $('#outgoingMessage').val();
+    $('#send').attr('disabled', (outgoingMessageValue.trim()).length > 0 ? false : true);
+  }
+
+  /*
+   When a user updates his/her name, let the server know by
+   emitting the "nameChange" event
+   */
+  function nameFocusOut() {
+    var name = $('#name').val();
+    socket.emit('nameChange', {id: sessionId, name: name});
+  }
+
+  /* Elements setup */
+  $('#outgoingMessage').on('keydown', outgoingMessageKeyDown);
+  $('#outgoingMessage').on('keyup', outgoingMessageKeyUp);
+  $('#name').on('focusout', nameFocusOut);
+  $('#send').on('click', sendMessage);
+
+}
+
+$(document).on('ready', init);
--- server.js
+++ server.js
@@ -1,54 +1,122 @@
-var express = require('express')
+/*
+ Module dependencies:
+
+ - Express
+ - Http (to run Express)
+ - Underscore (because it's cool)
+ - Socket.IO
+
+ It is a common practice to name the variables after the module name.
+ Ex: http is the "http" module, express is the "express" module, etc.
+ The only exception is Underscore, where we use, conveniently, an
+ underscore. Oh, and "socket.io" is simply called io. Seriously, the
+ rest should be named after its module name.
+
+ */
+var express = require("express")
   , app = express()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
-  , _ = require('underscore')
-  , participants = [];
+  , http = require("http").createServer(app)
+  , io = require("socket.io").listen(http)
+  , _ = require("underscore");
 
-app.set('ipaddr', process.env.INTERNAL_IP || "127.0.0.1");
-app.set('port', process.env.INTERNAL_PORT || 8080);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+/*
+ The list of participants in our chatroom.
+ The format of each participant will be:
+ {
+ id: "sessionId",
+ name: "participantName"
+ }
+ */
+var participants = []
+
+/* Server config */
+
+//Server's IP address
+app.set("ipaddr", "127.0.0.1");
+
+//Server's port number
+app.set("port", 8080);
+
+//Specify the views folder
+app.set("views", __dirname + "/views");
+
+//View engine is Jade
+app.set("view engine", "jade");
+
+//Specify where the static content is
+app.use(express.static("public", __dirname + "/public"));
+
+//Tells server to support JSON, urlencoded, and multipart requests
 app.use(express.bodyParser());
-app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-app.use(express.static('public', __dirname + 'public'));
 
-app.get('/', function(request, response) {
-  response.render('index');
+/* Server routing */
+
+//Handle route "GET /", as in "http://localhost:8080/"
+app.get("/", function(request, response) {
+
+  //Render the view called "index"
+  response.render("index");
+
 });
 
-app.post('/message', function(request, response) {
+//POST method to create a chat message
+app.post("/message", function(request, response) {
 
+  //The request body expects a param named "message"
   var message = request.body.message;
 
-  if(_.isEmpty(message.trim())) {
-    return response.json(400, {error: "Message is empty"});
+  //If the message is empty or wasn't sent it's a bad request
+  if(_.isUndefined(message) || _.isEmpty(message.trim())) {
+    return response.json(400, {error: "Message is invalid"});
   }
 
+  //We also expect the sender's name with the message
   var name = request.body.name;
 
-  io.sockets.emit('incomingMessage', {message: message, name: name});
-  response.send(200);
+  //Let our chatroom know there was a new message
+  io.sockets.emit("incomingMessage", {message: message, name: name});
+
+  //Looks good, let the client know
+  response.json(200, {message: "Message received"});
 
 });
 
-io.on('connection', function(socket){
+/* Socket.IO events */
+io.on("connection", function(socket){
 
-  socket.on('disconnect', function() {
-    participants = _.without(participants,_.findWhere(participants, {id: socket.id}));
-    io.sockets.emit('userDisconnected', {id: socket.id, sender:'system'});
-  });
-
-  socket.on('newUser', function(data) {
+  /*
+   When a new user connects to our server, we expect an event called "newUser"
+   and then we'll emit an event called "newConnection" with a list of all
+   participants to all connected clients
+   */
+  socket.on("newUser", function(data) {
     participants.push({id: data.id, name: data.name});
-    io.sockets.emit('newConnection', {participants: participants});
+    io.sockets.emit("newConnection", {participants: participants});
   });
 
-  socket.on('nameChange', function(data) {
+  /*
+   When a user changes his name, we are expecting an event called "nameChange"
+   and then we'll emit an event called "nameChanged" to all participants with
+   the id and new name of the user who emitted the original message
+   */
+  socket.on("nameChange", function(data) {
     _.findWhere(participants, {id: socket.id}).name = data.name;
-    io.sockets.emit('nameChanged', {id: data.id, name: data.name});
+    io.sockets.emit("nameChanged", {id: data.id, name: data.name});
+  });
+
+  /*
+   When a client disconnects from the server, the event "disconnect" is automatically
+   captured by the server. It will then emit an event called "userDisconnected" to
+   all participants with the id of the client that disconnected
+   */
+  socket.on("disconnect", function() {
+    participants = _.without(participants,_.findWhere(participants, {id: socket.id}));
+    io.sockets.emit("userDisconnected", {id: socket.id, sender:"system"});
   });
 
 });
 
-server.listen(app.get('port'), app.get('ipaddr'));
+//Start the http server at port and IP defined before
+http.listen(app.get("port"), app.get("ipaddr"), function() {
+  console.log("Server up and running. Go to http://" + app.get("ipaddr") + ":" + app.get("port"));
+});
